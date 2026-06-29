21976b44f9f5f0682408281c9312aca674385469
meshal10613 <syedmohiuddinmeshal24@gmail.com>
2026-06-29 11:11:51 +0600

    feat: User Input in C Programming Language

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 //? gcc main.c -o main && ./main
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main()
 {
@@ -61,6 +62,37 @@ int main()
 	// z/=3;
 
 	// printf("%.2f", z);
+
+	//* User Input
+	int age = 0;
+	float gpa = 1.0f;
+	char grade = '\0';
+	char firstName[5] = "";
+	char fullName[30] = "";
+
+	printf("Enter your age: ");
+	scanf("%d", &age);
+
+	printf("Enter your gpa: ");
+	scanf("%f", &gpa);
+
+	printf("Enter your grade: ");
+	scanf(" %c", &grade);
+
+	printf("Enter your first name: ");
+	scanf("%s", &firstName);
+
+	getchar(); // new line
+	printf("Enter your full name: ");
+	fgets(fullName, sizeof(fullName), stdin); // scan something with space & also fgets add a extra new line after the string end
+	fullName[strlen(fullName) - 1] = '\0'; // that's why we can use this to rid the new line
+
+	printf("----------------------------*----------------------------\n");
+	printf("Your full name is %s \n", fullName);
+	printf("Your age is %d \n", age);
+	printf("Your GPA is %.2f \n", gpa);
+	printf("Your grade is %c \n", grade);
+	printf("Your first name is %s \n", firstName);
 
 	return 0;
 }
